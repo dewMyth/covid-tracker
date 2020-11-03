adbd7b65e91c8d16027883f5c5d874e725ac7d2f
dewMyth <akalankadewmith@gmail.com>
2020-11-03 15:45:28 +0530

    Imported Covid 19 data from API as an Array

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { CountriesComponent } from './components/countries/countries.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HomeComponent,
-    CountriesComponent
+    CountriesComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/data-service.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { GlobalDataSummary } from '../models/GlobalDataSummary';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataServiceService {
+
+  private golbalDataUrl = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/11-02-2020.csv'
+
+  constructor(private http : HttpClient) { }
+
+  getGlobalData(){
+    return this.http.get(this.golbalDataUrl, {responseType : 'text'}).pipe(
+       map(result => {
+        let data: GlobalDataSummary[] = [];
+        let rows = result.split('\n');
+        rows.forEach( row => {
+          let cols = row.split(/,(?=\S)/);
+          console.log(cols);
+          data.push({
+            country : cols[3],
+            confirmed : parseInt(cols[7]),   //In type script we can use " + " instead of parseInt ---> Ex : +col(7)
+            deaths : parseInt(cols[8]),
+            recovered : +cols[9],
+            active : +cols[10], 
+          })
+        })
+
+        console.log(data);
+        return [];
+      })
+    )
+  }
+}
