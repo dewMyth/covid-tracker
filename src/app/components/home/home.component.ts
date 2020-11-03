c16841df6d2517e64bbfff23b6f97f8e8d912ef8
dewMyth <akalankadewmith@gmail.com>
2020-11-03 16:52:22 +0530

    Add global data summary to home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -10,14 +10,44 @@ export class HomeComponent implements OnInit {
 
   constructor(private dataService : DataServiceService) { }
 
-  ngOnInit(): void {
+
+  totalConfirmed = 0;
+  totalActive = 0;
+  totalDeaths = 0;
+  totalRecovered = 0;
+
+  strConfirmed = '';
+  strActive = '';
+  strDeaths = '';
+  strRecovered = '';
+  
+  numberWithCommas(number) {
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
+
+
+  ngOnInit() {
     this.dataService.getGlobalData().subscribe(
       {
         next : (result) => {
-          console.log(result);
+          result.forEach(cs=>{
+            if(!Number.isNaN(cs.confirmed)){
+              this.totalConfirmed += cs.confirmed,
+              this.totalActive += cs.active,
+              this.totalDeaths += cs.deaths,
+              this.totalRecovered += cs.recovered
+            }
+          })
+           this.strConfirmed = this.numberWithCommas(this.totalConfirmed);
+           this.strActive = this.numberWithCommas(this.totalActive);
+           this.strDeaths = this.numberWithCommas(this.totalDeaths);
+           this.strRecovered = this.numberWithCommas(this.totalRecovered);
         }
       }
     )
+     console.log(this.numberWithCommas(this.totalConfirmed));
   }
+
+ 
 
 }
--- src/app/services/data-service.service.ts
+++ src/app/services/data-service.service.ts
@@ -16,21 +16,30 @@ export class DataServiceService {
     return this.http.get(this.golbalDataUrl, {responseType : 'text'}).pipe(
        map(result => {
         let data: GlobalDataSummary[] = [];
+        let raw = {};
         let rows = result.split('\n');
         rows.splice(0,1);
         rows.forEach( row => {
           let cols = row.split(/,(?=\S)/);
-          data.push({
+          let csv = {
             country : cols[3],
             confirmed : parseInt(cols[7]),   //In type script we can use " + " instead of parseInt ---> Ex : +col(7)
             deaths : parseInt(cols[8]),
             recovered : +cols[9],
             active : +cols[10], 
-          })
+          }
+          let temp : GlobalDataSummary = raw[csv.country];
+          if(temp){
+            temp.confirmed = csv.confirmed + temp.confirmed
+            temp.deaths = csv.deaths + temp.deaths
+            temp.recovered = csv.recovered + temp.recovered
+            temp.active = csv.active + temp.active
+            raw[csv.country] = temp;
+          }else{
+            raw[csv.country] = csv;
+          }
         })
-
-        console.log(data);
-        return [];
+        return <GlobalDataSummary[]>Object.values(raw);
       })
     )
   }
